fe7565b08c50c8bcc271f2f14a240be7d2531a62
Dan Kelleher <kelleherjdan@gmail.com>
2023-11-06 10:59:51 -0500

    remove unneeded code

--- task_flows/utils.py
+++ task_flows/utils.py
@@ -1,41 +1,28 @@
 import json
 import re
 from subprocess import run
-from typing import Callable, Dict, List, Optional
+from typing import Callable, Dict, List, Literal, Optional, Sequence
 
 import click
+from alert_msgs import MsgDst
 from dynamic_imports import import_module_attr
-from paramiko import AutoAddPolicy, SSHClient
-
 from ezloggers import get_logger
+from pydantic import BaseModel
 
 logger = get_logger("task-flows", stdout=True)
 
 _FILE_PREFIX = "task_flow_"
 
 
-def ssh_client():
-    client = SSHClient()
-    client.set_missing_host_key_policy(AutoAddPolicy())
-    client.connect()
-    return client
+class Alerts(BaseModel):
+    send_to: Sequence[MsgDst]
+    send_on: Sequence[Literal["start", "error", "finish"]]
 
-
-def ftp_client():
-    # ftp_client.put(local / file / path, remote / file / path)
-    # ftp_client.close()
-    return ssh_client().open_sftp()
-
-
-def func_call_cmd(func: Callable, *args, **kwargs) -> str:
-    """Generate command to call function with optional args and kwargs."""
-    # TODO env arg with non-poetry stuff.
-    cmd = f"poetry run _tasks_flows_call {func.__module__} {func.__name__}"
-    if args:
-        cmd += f" --args {json.dumps(args)}"
-    if kwargs:
-        cmd += f" --kwargs {json.dumps(kwargs)}"
-    return cmd
+    def model_post_init(self, __context) -> None:
+        if not isinstance(self.send_to, (list, tuple)):
+            self.send_to = [self.send_to]
+        if isinstance(self.send_on, str):
+            self.send_on = [self.send_on]
 
 
 def parse_systemctl_tables(command: List[str]) -> List[Dict[str, str]]:
@@ -54,6 +41,17 @@ def parse_systemctl_tables(command: List[str]) -> List[Dict[str, str]]:
             line_data[match.group()] = field_text.strip()
         lines_data.append(line_data)
     return lines_data
+
+
+def func_call_cmd(func: Callable, *args, **kwargs) -> str:
+    """Generate command to call function with optional args and kwargs."""
+    # TODO env arg with non-poetry stuff.
+    cmd = f"poetry run _tasks_flows_call {func.__module__} {func.__name__}"
+    if args:
+        cmd += f" --args {json.dumps(args)}"
+    if kwargs:
+        cmd += f" --kwargs {json.dumps(kwargs)}"
+    return cmd
 
 
 @click.command()
