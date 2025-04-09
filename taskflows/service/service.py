8b7c8cf57609f1e1b06c22686e59a8d6805a765f
hetzner-server-user <kelleherjdan@gmail.com>
2025-04-09 22:42:35 +0000

    union type

--- taskflows/service/service.py
+++ taskflows/service/service.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from functools import cache
 from pathlib import Path
 from pprint import pformat
-from typing import Callable, Dict, List, Literal, Optional, Sequence, Set
+from typing import Callable, Dict, List, Literal, Optional, Sequence, Set, Union
 
 import cloudpickle
 import dbus
@@ -19,8 +19,8 @@ from .docker import DockerContainer, delete_docker_container
 from .exec import deserialize_and_call
 from .schedule import Schedule
 
-ServiceT = str | "Service"
-ServicesT = ServiceT | Sequence[ServiceT]
+ServiceT = Union[str | "Service"]
+ServicesT = Union[ServiceT | Sequence[ServiceT]]
 
 systemd_dir = Path.home().joinpath(".config", "systemd", "user")
 
