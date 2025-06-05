5c8a03ea4adeb1866a9f58cd02c3e7305bd840b7
hetzner-server-user <kelleherjdan@gmail.com>
2025-06-05 23:27:10 +0000

    remove old imports

--- taskflows/__init__.py
+++ taskflows/__init__.py
@@ -6,11 +6,5 @@ _SYSTEMD_FILE_PREFIX = "taskflow-"
 
 from alert_msgs import EmailAddrs, SlackChannel
 
-from .common import (
-    HTTPClient,
-    HTTPResponse,
-    ShutdownHandler,
-    get_http_client,
-    get_shutdown_handler,
-)
+from .common import ShutdownHandler, get_shutdown_handler
 from .tasks import Alerts, task
